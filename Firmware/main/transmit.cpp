7b89ce318244efc6bc8419f62cfb808f5d1d1eef
maurinElectroTextile <maurin@etextile.org>
2021-01-13 10:59:36 +0100

    BUGFIX MIDI mapping

--- Firmware/main/transmit.cpp
+++ Firmware/main/transmit.cpp
@@ -11,17 +11,23 @@ void USB_SLIP_OSC_SETUP() {
   SLIPSerial.begin(BAUD_RATE); // FIXME
 }
 
-void blobs_usb_slipOsc(blob_t* blob_ptr, preset_t* preset_ptr, OSCBundle* synthOSC) {
+void blobs_usb_slipOsc(llist_t* blobs_ptr, preset_t* preset_ptr) {
 
-  OSCMessage msg("/b");
-  msg.add((uint8_t)blob_ptr->UID);                        // uint8_t unique session ID
-  msg.add((uint8_t)blob_ptr->alive);                      // uint8_t
-  msg.add((float)blob_ptr->centroid.X);                   // float
-  msg.add((float)blob_ptr->centroid.Y);                   // float
-  msg.add((uint8_t)blob_ptr->box.W);                      // uint8_t
-  msg.add((uint8_t)blob_ptr->box.H);                      // uint8_t
-  msg.add((uint8_t)(blob_ptr->box.D - preset_ptr->val));  // uint8_t
-  synthOSC->add(msg);
+  OSCBundle OSCbundle;
+  for (blob_t* blob_ptr = ITERATOR_START_FROM_HEAD(blobs_ptr); blob_ptr != NULL; blob_ptr = ITERATOR_NEXT(blob_ptr)) {
+    OSCMessage msg("/b");
+    msg.add(blob_ptr->UID);
+    msg.add(blob_ptr->alive);
+    msg.add(blob_ptr->centroid.X);
+    msg.add(blob_ptr->centroid.Y);
+    msg.add(blob_ptr->box.W);
+    msg.add(blob_ptr->box.H);
+    msg.add(blob_ptr->box.D - preset_ptr->val);
+    OSCbundle.add(msg);
+  }
+  SLIPSerial.beginPacket();     //
+  OSCbundle.send(SLIPSerial);   // Send the bytes to the SLIP stream
+  SLIPSerial.endPacket();       // Mark the end of the OSC Packet
 }
 #endif
 
@@ -31,44 +37,49 @@ void USB_MIDI_SETUP() {
 }
 
 // Send blobs values using ControlChange MIDI format
-// Separate blob's values to allow the mapping into Max4Live
-void blob_usb_midi_learn(llist_t* blobs_ptr, preset_t* preset_ptr) {
+// Send only the last blob that have been added to the sensor surface
+// Separate blob's values according to the encoder position to allow the mapping into Max4Live
+void blobs_usb_midi_learn(llist_t* blobs_ptr, preset_t* preset_ptr) {
 
-  for (blob_t* blob = ITERATOR_START_FROM_HEAD(blobs_ptr); blob != NULL; blob = ITERATOR_NEXT(blob)) {
-    switch (preset_ptr->val) {
-      case 1:
-        usbMIDI.sendControlChange(1, blob->alive, blobs_ptr->index + 1);
-        break;
-      case 2:
-        usbMIDI.sendControlChange(2, (uint8_t)round(blob->centroid.X), blobs_ptr->index + 1);
-        break;
-      case 3:
-        usbMIDI.sendControlChange(3, (uint8_t)round(blob->centroid.Y), blobs_ptr->index + 1);
-        break;
-      case 4:
-        usbMIDI.sendControlChange(4, blob->box.W, blobs_ptr->index + 1);
-        break;
-      case 5:
-        usbMIDI.sendControlChange(5, blob->box.H, blobs_ptr->index + 1);
-        break;
-      case 6:
-        usbMIDI.sendControlChange(6, constrain(blob->box.D, 0, 127), blobs_ptr->index + 1);
-        break;
+  for (blob_t* blob_ptr = ITERATOR_START_FROM_HEAD(blobs_ptr); blob_ptr != NULL; blob_ptr = ITERATOR_NEXT(blob_ptr)) {
+
+    if (blob_ptr->UID == blobs_ptr->index) {
+
+      switch (preset_ptr->val) {
+        case 1:
+          usbMIDI.sendControlChange(1, blob_ptr->alive, blobs_ptr->index + 1);
+          break;
+        case 2:
+          usbMIDI.sendControlChange(2, (uint8_t)round(blob_ptr->centroid.X), blobs_ptr->index + 1);
+          break;
+        case 3:
+          usbMIDI.sendControlChange(3, (uint8_t)round(blob_ptr->centroid.Y), blobs_ptr->index + 1);
+          break;
+        case 4:
+          usbMIDI.sendControlChange(4, blob_ptr->box.W, blobs_ptr->index + 1);
+          break;
+        case 5:
+          usbMIDI.sendControlChange(5, blob_ptr->box.H, blobs_ptr->index + 1);
+          break;
+        case 6:
+          usbMIDI.sendControlChange(6, constrain(blob_ptr->box.D, 0, 127), blobs_ptr->index + 1);
+          break;
+      }
     }
   }
   while (usbMIDI.read()); // Read and discard any incoming MIDI messages
 }
 
 // Send all blobs values using ControlChange MIDI format
-void blob_usb_midi_play(llist_t* blobs_ptr) {
+void blobs_usb_midi_play(llist_t* blobs_ptr) {
 
-  for (blob_t* blob = ITERATOR_START_FROM_HEAD(blobs_ptr); blob != NULL; blob = ITERATOR_NEXT(blob)) {
-    usbMIDI.sendControlChange(1, blob->alive, blob->UID + 1);
-    usbMIDI.sendControlChange(2, (uint8_t)round(blob->centroid.X), blob->UID + 1);
-    usbMIDI.sendControlChange(3, (uint8_t)round(blob->centroid.Y), blob->UID + 1);
-    usbMIDI.sendControlChange(4, blob->box.W, blob->UID + 1);
-    usbMIDI.sendControlChange(5, blob->box.H, blob->UID + 1);
-    usbMIDI.sendControlChange(6, constrain(blob->box.D, 0, 127), blob->UID + 1);
+  for (blob_t* blob_ptr = ITERATOR_START_FROM_HEAD(blobs_ptr); blob_ptr != NULL; blob_ptr = ITERATOR_NEXT(blob_ptr)) {
+    usbMIDI.sendControlChange(1, blob_ptr->alive, blob_ptr->UID + 1);
+    usbMIDI.sendControlChange(2, (uint8_t)round(blob_ptr->centroid.X), blob_ptr->UID + 1);
+    usbMIDI.sendControlChange(3, (uint8_t)round(blob_ptr->centroid.Y), blob_ptr->UID + 1);
+    usbMIDI.sendControlChange(4, blob_ptr->box.W, blob_ptr->UID + 1);
+    usbMIDI.sendControlChange(5, blob_ptr->box.H, blob_ptr->UID + 1);
+    usbMIDI.sendControlChange(6, constrain(blob_ptr->box.D, 0, 127), blob_ptr->UID + 1);
   }
   while (usbMIDI.read()); // Read and discard any incoming MIDI messages
 }
--- Firmware/main/transmit.h
+++ Firmware/main/transmit.h
@@ -7,6 +7,11 @@
 #ifndef __TRANSMIT_H__
 #define __TRANSMIT_H__
 
+#include "config.h"
+#include "presets.h"
+#include "llist.h"
+#include "blob.h"
+
 #if USB_MIDI
 #include <MIDI.h>                   // http://www.pjrc.com/teensy/td_midi.html
 #endif
@@ -16,13 +21,8 @@
 #include <OSCMessage.h>             // https://github.com/CNMAT/OSC
 #include <OSCBundle.h>              // https://github.com/CNMAT/OSC
 #include <SLIPEncodedUSBSerial.h>   // https://github.com/CNMAT/OSC
-//SLIPEncodedUSBSerial SLIPSerial(thisBoardsSerialUSB); // FIXME
+SLIPEncodedUSBSerial SLIPSerial(thisBoardsSerialUSB); // FIXME
 #endif
-
-#include "config.h"
-#include "presets.h"
-#include "llist.h"
-#include "blob.h"
 
 typedef struct preset preset_t; // Forward declaration
 typedef struct llist llist_t;   // Forward declaration
@@ -30,13 +30,13 @@ typedef struct blob blob_t;     // Forward declaration
 
 #if USB_SLIP_OSC
 void USB_SLIP_OSC_SETUP(void);
-void blob_usb_slipOsc(blob_t* blob_ptr, preset_t* preset_ptr, OSCBundle* synthOSC);
+void blobs_usb_slipOsc(llist_t* blobs_ptr, preset_t* preset_ptr);
 #endif
 
 #if USB_MIDI
 void USB_MIDI_SETUP(void);
-void blob_usb_midi_learn(llist_t* blobs_ptr, preset_t* preset_ptr);
-void blob_usb_midi_play(llist_t* blobs_ptr);
+void blobs_usb_midi_learn(llist_t* blobs_ptr, preset_t* preset_ptr);
+void blobs_usb_midi_play(llist_t* blobs_ptr);
 #endif
 
 #endif /*__TRANSMIT_H__*/
