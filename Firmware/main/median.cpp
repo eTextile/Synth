d4c4f84e7e0a89eb1ab688dcf647b39858ee9e82
maurinElectroTextile <maurin@etextile.org>
2021-01-22 21:33:54 +0100

    Added median filter

---
+++ Firmware/main/median.cpp
@@ -0,0 +1,59 @@
+/*
+  median filter (c) 2019 by RainerU
+  This code is under the MIT License
+  https://github.com/RainerU/medianfilter
+
+  Added multy blob filtering capability
+  Copyright (c) 2021 Maurin Donneaud <maurin@etextile.org>
+*/
+
+#include "median.h"
+
+void median(llist_t* blobs_ptr, median_t* storage_ptr) {
+
+  for (blob_t* blob_ptr = ITERATOR_START_FROM_HEAD(blobs_ptr); blob_ptr != NULL; blob_ptr = ITERATOR_NEXT(blob_ptr)) {
+
+    float inputVal = blob_ptr->box.D; // The new value
+    float outputVal = inputVal;       // The new value could be the median
+
+    // Initialize all arrays
+    if (blob_ptr->alive && storage_ptr[blob_ptr->UID].init) {
+      storage_ptr[blob_ptr->UID].init = false;                    // Run initialization once
+      for (int i = 0; i < TIME_WINDOW; i++) {
+        storage_ptr[blob_ptr->UID].val[i] = blob_ptr->box.D;      // Fill array with new value
+        storage_ptr[blob_ptr->UID].sort[i] = 0;
+      }
+    }
+    // Store new value
+    else {
+      if (storage_ptr[blob_ptr->UID].index++ >= TIME_WINDOW) storage_ptr[blob_ptr->UID].index = 0;  // Go one step in ring storage
+      float oldVal = storage_ptr[blob_ptr->UID].val[storage_ptr[blob_ptr->UID].index];              // Save old value
+      float oldOrd = storage_ptr[blob_ptr->UID].sort[storage_ptr[blob_ptr->UID].index];             // Save old order number
+      storage_ptr[blob_ptr->UID].val[storage_ptr[blob_ptr->UID].index] = blob_ptr->box.D;           // Store new value
+      storage_ptr[blob_ptr->UID].sort[storage_ptr[blob_ptr->UID].index] = 0;                        // Reset order number for new value
+
+      // Loop
+      int c = storage_ptr[blob_ptr->UID].index;                                 // Index for loop through ring storage
+      if (++c >= TIME_WINDOW) c = 0;                                            // Go one step (don't process index position)
+      do {
+        if (blob_ptr->box.D <= storage_ptr[blob_ptr->UID].val[c] &&
+            oldOrd > storage_ptr[blob_ptr->UID].sort[c])
+          storage_ptr[blob_ptr->UID].sort[c]++;                                 // Remove bigger value, add smaller value
+        else if (blob_ptr->box.D > storage_ptr[blob_ptr->UID].val[c] &&
+                 oldOrd < storage_ptr[blob_ptr->UID].sort[c])
+          storage_ptr[blob_ptr->UID].sort[c]--;                                 // Remove smaller value, add bigger value
+        if (storage_ptr[blob_ptr->UID].sort[c] == MEDIAN_POS)
+          outputVal = storage_ptr[blob_ptr->UID].val[c];                        // Median found
+        if (blob_ptr->box.D > storage_ptr[blob_ptr->UID].val[c])
+          storage_ptr[blob_ptr->UID].sort[storage_ptr[blob_ptr->UID].index]++;  // Compute new value order
+        if (++c >= TIME_WINDOW) c = 0;                                          // Go one step in ring storage
+      } while (c != storage_ptr[blob_ptr->UID].index);                          // Stop at index position
+    }
+
+    if (!blob_ptr->alive) {
+      storage_ptr[blob_ptr->UID].init = true;                                   // Can run initialization again
+    }
+    blob_ptr->box.D = outputVal;                                                // Replace the value with the computed median value
+    Serial.printf("\n%f ; %f", inputVal, outputVal);
+  }
+}
---
+++ Firmware/main/median.h
@@ -0,0 +1,32 @@
+/*
+  median filter (c) 2019 by RainerU
+  This code is under the MIT License
+  https://github.com/RainerU/medianfilter
+
+  Added multy blob filtering capability
+  Copyright (c) 2021 Maurin Donneaud <maurin@etextile.org>
+*/
+
+#ifndef __MEDIAN_H__
+#define __MEDIAN_H__
+
+#include "config.h"
+#include "llist.h"
+#include "blob.h"
+
+typedef struct llist llist_t;  // Forward declaration
+typedef struct blob blob_t;    // Forward declaration
+
+#define TIME_WINDOW 7                  // Allowed filter window size : 3, 5, 7...
+#define MEDIAN_POS ((TIME_WINDOW-1)/2) // position of median in ordered list
+
+typedef struct {
+  boolean init;             // Init flag
+  float val[TIME_WINDOW];  // Array of values
+  float sort[TIME_WINDOW]; // Array of order numbers
+  int index;                // Current index of the ring storage
+} median_t;
+
+void median(llist_t* blobs_ptr, median_t* storage_ptr);
+
+#endif /*__MEDIAN_H__*/
