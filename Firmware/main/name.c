72a0ef00a27576fe0ce7e815e21a43fa02209e4f
maurinElectroTextile <maurin@etextile.org>
2021-01-22 21:39:10 +0100

    Minor changes

--- Firmware/main/name.c
+++ Firmware/main/name.c
@@ -1,12 +1,12 @@
-#ifdef USB_MIDI
+#ifdef MIDI_USB
 #include "usb_names.h"
 
 #define MIDI_NAME {'E','T','E','X','T','I','L','E','_','S','Y','N','T','H'}
 #define MIDI_NAME_LEN  14
 
 struct usb_string_descriptor_struct usb_string_product_name = {
-        2 + MIDI_NAME_LEN * 2,
-        3,
-        MIDI_NAME
+  2 + MIDI_NAME_LEN * 2,
+  3,
+  MIDI_NAME
 };
 #endif
