df55eee887bb9f9aa1fb13b4923622742f9ffd26
maurinElectroTextile <maurin@etextile.org>
2025-04-09 08:42:17 +0200

    Added TODO

--- Firmware/src/mapping.cpp
+++ Firmware/src/mapping.cpp
@@ -10,27 +10,32 @@ llist_t llist_mappings;
 
 void mapping_lib_update(void) {
 
-  for (lnode_t* blob_node_ptr = ITERATOR_START_FROM_HEAD(&llist_blobs); blob_node_ptr != NULL; blob_node_ptr = ITERATOR_NEXT(blob_node_ptr)) {
-    blob_t* blob_ptr = (blob_t*)ITERATOR_DATA(blob_node_ptr);
+  for (lnode_t* mapping_node_ptr = ITERATOR_START_FROM_HEAD(&llist_mappings); mapping_node_ptr != NULL; mapping_node_ptr = ITERATOR_NEXT(mapping_node_ptr)) {
+    common_t* mapping_ptr = (common_t*)ITERATOR_DATA(mapping_node_ptr);
+    
+    for (lnode_t* blob_node_ptr = ITERATOR_START_FROM_HEAD(&llist_blobs); blob_node_ptr != NULL; blob_node_ptr = ITERATOR_NEXT(blob_node_ptr)) {
+      blob_t* blob_ptr = (blob_t*)ITERATOR_DATA(blob_node_ptr);
 
-    for (lnode_t* mapping_node_ptr = ITERATOR_START_FROM_HEAD(&llist_mappings); mapping_node_ptr != NULL; mapping_node_ptr = ITERATOR_NEXT(mapping_node_ptr)) {
-      common_t* mapping_ptr = (common_t*)ITERATOR_DATA(mapping_node_ptr);
-      
       if (mapping_ptr->is_blob_inside_func_ptr(mapping_ptr, blob_ptr)) {
+
         if (blob_ptr->action.mapping_ptr == NULL) {
           if (mapping_ptr->blob_assign_func_ptr(mapping_ptr, blob_ptr)){
             mapping_ptr->start_func_ptr(blob_ptr);
+            Serial.printf("\n_ASSIGN");
           }
         }
         else {
           if (blob_ptr->status == PRESENT && blob_ptr->last_status == RELEASED) {
             mapping_ptr->start_func_ptr(blob_ptr);
+            Serial.printf("\n_START");
           }
           else if (blob_ptr->status == PRESENT) {
             mapping_ptr->continue_func_ptr(blob_ptr);
+            Serial.printf("\n_PRESENT");
           }
           else if (blob_ptr->status == RELEASED && blob_ptr->last_status == MISSING) {
             mapping_ptr->stop_func_ptr(blob_ptr);
+            Serial.printf("\n_RELEASED");
           }
         }
       }
